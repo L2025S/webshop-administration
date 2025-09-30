2e13b8a0e457bbe8f53da98e0da25bb5074f3ca9
L.W. <reaktor.utfallen-2z@icloud.com>
2025-09-30 18:38:05 +0200

    added "Clothes addNew()"

--- src/main/java/se/iths/lw/Clothes.java
+++ src/main/java/se/iths/lw/Clothes.java
@@ -1,12 +1,13 @@
 package se.iths.lw;
 
+import javax.swing.*;
 import java.util.Locale;
 
 class Clothes extends Product {
     private String size;
     private String color;
 
-    Clothes (String articleNumber, String title, String size, String color, double price,String description, int stock ) {
+    Clothes (String articleNumber, String title, String size, String color, double price, String description, int stock ) {
         super (articleNumber, title, price, description, stock);
         this.size = size;
         this.color = color;
@@ -31,6 +32,26 @@ class Clothes extends Product {
 
     @Override
     public String toCsvFormat() {
-        return String.format (Locale.US, "%s, %s, %s, %s, %.2f, %s, %d", getArticleNumber(), getTitle(),size, color, getPrice(),getDescription(), getStock());
+        return String.format (Locale.US, "%s, %s, %s, %s, %.2f, %s, %d", getArticleNumber(), getTitle(),size, color, getPrice(), getDescription(), getStock());
+    }
+
+    @Override
+    Clothes addNew() {
+        Clothes clothes = new Clothes (getArticleNumber(), getTitle(), size, color, getPrice(), getDescription(), getStock());
+        String articleNumber = JOptionPane.showInputDialog("Enter Article Number:");
+        clothes.setArticleNumber(articleNumber);
+        String title = JOptionPane.showInputDialog("Enter Title: ");
+        clothes.setTitle(title);
+        String size = (JOptionPane.showInputDialog("Enter Size: e.g X,M,or L")).toUpperCase();
+        clothes.setSize(size);
+        String color = JOptionPane.showInputDialog("Enter Color: ");
+        clothes.setColor(color);
+        double price = Double.parseDouble(JOptionPane.showInputDialog("Enter the price"));
+        clothes.setPrice(price);
+        String description = JOptionPane.showInputDialog("Enter the description");
+        clothes.setDescription(description);
+        int stock = Integer.parseInt(JOptionPane.showInputDialog("Enter the stock"));
+        clothes.setStock(stock);
+        return clothes;
     }
 }
