dd96236434de42df0a403a33373cb90c7b234e87
L.W. <reaktor.utfallen-2z@icloud.com>
2025-10-01 12:20:21 +0200

    Added "Lista alla produkter"

--- src/main/java/se/iths/lw/Main.java
+++ src/main/java/se/iths/lw/Main.java
@@ -6,11 +6,14 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 
 class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Object [] functions = { "Add product", "List all products", "Show product's info.", "Exit application"};
+        Object[] functions = {"Add product", "List all products", "Show product's info.", "Exit application"};
         Object selectedFunction = JOptionPane.showInputDialog(
                 null,
                 "Choose the function",
@@ -22,8 +25,8 @@ class Main {
         );
         switch (selectedFunction.toString()) {
             case "Add product":
-                Object [] addProduct = {"Add Book", "Add Clothes", "Add Food"};
-                Object selectedProduct =JOptionPane.showInputDialog (
+                Object[] addProduct = {"Add Book", "Add Clothes", "Add Food"};
+                Object selectedProduct = JOptionPane.showInputDialog(
                         null,
                         "Choose the category",
                         "Category",
@@ -32,52 +35,56 @@ class Main {
                         addProduct,
                         addProduct[0]
                 );
-                switch(selectedProduct.toString()){
-                    case"Add Book":
-                        Book book = new Book ("articleNumber", "title", 0.0, "description", 0, "author");
+                switch (selectedProduct.toString()) {
+                    case "Add Book":
+                        Book book = new Book("articleNumber", "title", 0.0, "description", 0, "author");
                         book.addNew();
                         book.toCsvFormat();
                         String content = book.toCsvFormat();
-                        Path file = Path.of ("webshop.csv");
+                        Path file = Path.of("webshop.csv");
                         try {
                             Files.writeString(file, System.lineSeparator() + content, StandardOpenOption.CREATE, StandardOpenOption.APPEND);
                         } catch (IOException e) {
                             System.out.println(e.getMessage());
                         }
                     case "Add Clothes":
-                        Clothes clothes = new Clothes ("articleNumber", "title",0.0, "description", 0, "size", "color");
+                        Clothes clothes = new Clothes("articleNumber", "title", 0.0, "description", 0, "size", "color");
                         clothes.addNew();
                         content = clothes.toCsvFormat();
-                        file = Path.of ("webshop.csv");
+                        file = Path.of("webshop.csv");
                         try {
-                            Files.writeString(file, System.lineSeparator()+ content, StandardOpenOption.CREATE,StandardOpenOption.APPEND);
-                        } catch(IOException e) {
+                            Files.writeString(file, System.lineSeparator() + content, StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+                        } catch (IOException e) {
                             System.out.println(e.getMessage());
                         }
                     case "Add Food":
-                        Food food = new Food ("articleNumber", "title", 0.0, "description", 0, LocalDate.parse ("2025-10-01"));
+                        Food food = new Food("articleNumber", "title", 0.0, "description", 0, LocalDate.parse("2025-10-01"));
                         food.addNew();
                         content = food.toCsvFormat();
                         file = Path.of("webshop.csv");
                         try {
-                            Files.writeString(file,System.lineSeparator()+content, StandardOpenOption.CREATE,StandardOpenOption.APPEND);
+                            Files.writeString(file, System.lineSeparator() + content, StandardOpenOption.CREATE, StandardOpenOption.APPEND);
                         } catch (IOException e) {
                             System.out.println(e.getMessage());
                         }
                 }
-            break;
+                break;
             case "List all products":
+                List<String> lines;
+                lines = Files.readAllLines(Path.of("webshop.csv"));
+                for (String line : lines) {
+                    System.out.println(line);
+                }
                 break;
-            case "Show product's info":
-                break;
-            case "Exit application":
-                System.exit(0);
 
+            case "Show product's info":
+                        break;
+            case "Exit application":
+                        System.exit(0);
 
 
 
 
         }
-
     }
 }
